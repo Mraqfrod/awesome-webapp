3186f82dc16867ee1d805b6844947fcb61820b97
hehui <mraqfrod@163.com>
2017-09-01 14:40:55 +0800

    测试

---
+++ www/test_user.py
@@ -0,0 +1,13 @@
+from www import orm
+from www.models import User
+
+
+def test():
+    yield from orm.create_pool(user='www-data', password='www-data', database='awesome')
+
+    u = User(name='Test', email='test@example.com', passwd='1234567890', image='about:blank')
+
+    yield from u.save()
+
+for x in test():
+    pass
