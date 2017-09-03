b55c511adb682c60dc46b76b9bfc1487c24d31e3
Mraqfrod <357181667@qq.com>
2017-09-03 21:53:54 +0800

    Web框架

--- www/coroweb.py
+++ www/coroweb.py
@@ -1,6 +1,15 @@
 import asyncio
+import inspect
 
 
 @asyncio.corotine
 def handle_url_xxx(request):
     pass
+
+def get_required_kw_args(fn):
+    args = []
+    params = inspect.signature(fn).parameters
+    for name, param in params.items():
+        if param.kind == inspect.Parameter.KEYWORD_ONLY and param.default == inspect.Parameter.empty:
+            args.append(name)
+    return tuple(args)
