c387f40714d1765c4f0414e35901397b2835c31b
hehui <mraqfrod@163.com>
2017-09-04 16:17:13 +0800

    updated

--- www/coroweb.py
+++ www/coroweb.py
@@ -1,4 +1,5 @@
 import asyncio
+import functools
 import inspect
 
 
@@ -13,3 +14,16 @@ def get_required_kw_args(fn):
         if param.kind == inspect.Parameter.KEYWORD_ONLY and param.default == inspect.Parameter.empty:
             args.append(name)
     return tuple(args)
+
+def post(path):
+    '''
+    Define decorator @post('/path')
+    '''
+    def decorator(func):
+        @functools.wraps(func)
+        def wrapper(*args, **kw):
+            return func(*args, **kw)
+        wrapper.__method__ = 'POST'
+        wrapper.__route__ = path
+        return wrapper
+    return decorator
