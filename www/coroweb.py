4fab9a90c095b21abd0b2a0080a8423d7147c424
hehui <mraqfrod@163.com>
2017-09-07 10:20:29 +0800

    updated

--- www/coroweb.py
+++ www/coroweb.py
@@ -7,14 +7,6 @@ import inspect
 def handle_url_xxx(request):
     pass
 
-def get_required_kw_args(fn):
-    args = []
-    params = inspect.signature(fn).parameters
-    for name, param in params.items():
-        if param.kind == inspect.Parameter.KEYWORD_ONLY and param.default == inspect.Parameter.empty:
-            args.append(name)
-    return tuple(args)
-
 def get(path):
     '''
     Define decorator @get('/path')
@@ -40,3 +32,20 @@ def post(path):
         wrapper.__route__ = path
         return wrapper
     return decorator
+
+def get_required_kw_args(fn):
+    args = []
+    params = inspect.signature(fn).parameters
+    for name, param in params.items():
+        if param.kind == inspect.Parameter.KEYWORD_ONLY and param.default == inspect.Parameter.empty:
+            args.append(name)
+    return tuple(args)
+
+def get_named_kw_args(fn):
+    args = []
+    params = inspect.signature(fn).parameters
+    for name, param in params.items():
+        if param.kind == inspect.Parameter.KEYWORD_ONLY:
+            args.append(name)
+    return tuple(args)
+
