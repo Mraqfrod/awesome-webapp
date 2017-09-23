49c42d9022622715e7ac1280564b3ec73930728a
Mraqfrod <357181667@qq.com>
2017-09-23 19:15:57 +0800

    commit

--- www/apis.py
+++ www/apis.py
@@ -10,3 +10,10 @@ class APIError(Exception):
         self.error = error
         self.data = data
         self.message = message
+
+class APIValueError(APIError):
+    '''
+    Indicate the input value has error or invalid. The data specifies the error field of input form.
+    '''
+    def __init__(self, field, message=''):
+        super(APIValueError, self).__init__('value:invalid', field, message)
