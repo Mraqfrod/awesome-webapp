509a0c8f3b8414ae426f0b8ac25e4294de369297
Mraqfrod <357181667@qq.com>
2017-09-25 19:44:00 +0800

    updated

--- www/apis.py
+++ www/apis.py
@@ -17,3 +17,10 @@ class APIValueError(APIError):
     '''
     def __init__(self, field, message=''):
         super(APIValueError, self).__init__('value:invalid', field, message)
+
+class APIResourceNotFoundError(APIError):
+    '''
+    Indicate the resource was not found. The data specifies the resource name.
+    '''
+    def __init__(self, field, message=''):
+        super(APIResourceNotFoundError, self).__init__('value:notfound', field, message)
