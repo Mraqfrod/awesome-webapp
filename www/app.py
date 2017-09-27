36831aa2758bf7703b33545aedd7a911bca47d55
hehui <mraqfrod@163.com>
2017-09-27 10:17:53 +0800

    commit

--- www/app.py
+++ www/app.py
@@ -1,7 +1,29 @@
 import logging
 import asyncio
-from aiohttp import web
+from aiohttp import web, os
+from jinja2 import Environment, FileSystemLoader
 
+
+def init_jinja2(app, **kw):
+    logging.info('init jinja2...')
+    options = dict(
+        autoescape = kw.get('autoescape', True),
+        block_start_string = kw.get('block_start_string', '{%'),
+        block_end_string = kw.get('block_end_string', '%}'),
+        variable_start_string = kw.get('variable_start_string', '{{'),
+        variable_end_string = kw.get('variable_end_string', '}}'),
+        auto_reload = kw.get('auto_reload', True)
+    )
+    path = kw.get('path', None)
+    if path is None:
+        path = os.path.join(os.path.dirname(os.path.abspath(__file__)), 'templates')
+    logging.info('set jinja2 template path: %s' % path)
+    env = Environment(loader=FileSystemLoader(path), **options)
+    filters = kw.get('filters', None)
+    if filters is not None:
+        for name, f in filters.items():
+            env.filters[name] = f
+    app['__templating__'] = env
 
 def index(request):
     return web.Response(body=b'<h1>Awesome</h1>',content_type='text/html')
