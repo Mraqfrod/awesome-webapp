02d4d31ac1872372950989a618fad280fdf9679b
hehui <mraqfrod@163.com>
2017-11-20 10:30:05 +0800

    commit

--- fabfile.py
+++ fabfile.py
@@ -72,3 +72,50 @@ def deploy():
         sudo('/etc/init.d/nginx reload')
 
 RE_FILES = re.compile('\r?\n')
+
+def rollback():
+    '''
+    rollback to previous version
+    '''
+    with cd(_REMOTE_BASE_DIR):
+        r = run('ls -p -1')
+        files = [s[:-1] for s in RE_FILES.split(r) if s.startswith('www-') and s.endswith('/')]
+        files.sort(cmp=lambda s1, s2: 1 if s1 < s2 else -1)
+        r = run('ls -l www')
+        ss = r.split(' -> ')
+        if len(ss) != 2:
+            print ('ERROR: \'www\' is not a symbol link.')
+            return
+        current = ss[1]
+        print ('Found current symbol link points to: %s\n' % current)
+        try:
+            index = files.index(current)
+        except ValueError :
+            print ('ERROR: symbol link is invalid.')
+            return
+        if len(files) == index + 1:
+            print ('ERROR: already the oldest version.')
+        old = files[index + 1]
+        print ('==================================================')
+        for f in files:
+            if f == current:
+                print ('      Current ---> %s' % current)
+            elif f == old:
+                print ('  Rollback to ---> %s' % old)
+            else:
+                print ('                   %s' % f)
+        print ('==================================================')
+        print ('')
+        yn = input('continue? y/N ')
+        if yn != 'y' and yn != 'Y':
+            print ('Rollback cancelled.')
+            return
+        print ('Start rollback...')
+        sudo('rm -f www')
+        sudo('ln -s %s www' % old)
+        sudo('chown www-data:www-data www')
+        with settings(warn_only=True):
+            sudo('supervisorctl stop awesome')
+            sudo('supervisorctl start awesome')
+            sudo('/etc/init.d/nginx reload')
+        print ('ROLLBACKED OK.')
